0a0acfbc8e24f5fd81e5cf26a7506a3d114f0e0c
zavalita1 <mkmcepiga@gmail.com>
2025-09-19 00:32:02 +0200

    fix missing kv bits stopping the startup

--- Filmowanie.Database/Extensions/ServiceCollectionExtensions.cs
+++ Filmowanie.Database/Extensions/ServiceCollectionExtensions.cs
@@ -64,7 +64,7 @@ public static class ServiceCollectionExtensions
         var credential = new DefaultAzureCredential();
         var blobUrl = configuration["DataProtectionKeysBlobUrl"];
         var keyVaultName = configuration["KeyVaultName"];
-        var keyId = configuration["https://kv-filmowanie2.vault.azure.net/keys/dataprotectionkey/3843c98c4a2a411892e010f92ac0f205"];
+       // var keyId = configuration["https://kv-filmowanie2.vault.azure.net/keys/dataprotectionkey/3843c98c4a2a411892e010f92ac0f205"];
 
         if (string.IsNullOrEmpty(blobUrl))
             throw new ConfigurationErrorsException("Missing data-protection blob url!");
@@ -72,8 +72,8 @@ public static class ServiceCollectionExtensions
         if (string.IsNullOrEmpty(keyVaultName))
             throw new ConfigurationErrorsException("Missing keyvault name in config!");
 
-        if (string.IsNullOrEmpty(keyId))
-            throw new ConfigurationErrorsException("Missing key identifier in config!");
+        // if (string.IsNullOrEmpty(keyId))
+        //     throw new ConfigurationErrorsException("Missing key identifier in config!");
 
         var kvUri = $"https://{keyVaultName}.vault.azure.net";
 
